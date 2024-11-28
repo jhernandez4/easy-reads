9d0eeec2436b9d205db2e61f6ff75dbf516593a5
jhernandez4 <jokhernandez120744@gmail.com>
2024-11-27 19:57:50 -0800

    Import updated tables

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI, Depends, HTTPException, Query
-from database import get_session, create_db_and_tables, engine, Textbook, Chapter, Quiz
 from typing import Annotated
 from sqlmodel import Session, select
+from database import (
+    get_session, create_db_and_tables, engine, Textbook, Chapter, 
+    Conversation, Response, Quiz, Question
+)
 
 # Dependency for session management
 SessionDep = Annotated[Session, Depends(get_session)]
