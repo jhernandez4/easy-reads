a750c85ae35f6ce606005896a1896a5b52274ae6
jhernandez4 <jokhernandez120744@gmail.com>
2024-12-06 16:36:06 -0800

    Load environment variable correctly

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-from fastapi import FastAPI, Depends, HTTPException, Query
+from fastapi import FastAPI, Depends, HTTPException, Query, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jwt.exceptions import InvalidTokenError
+from passlib.context import CryptContext
+from pydantic import BaseModel
 from typing import Annotated
 from sqlmodel import Session, select
 import google.generativeai as genai
@@ -6,8 +10,13 @@ from database import (
     get_session, create_db_and_tables, engine, Textbook, Chapter, 
     Conversation, Response, Quiz, Question
 )
+from dotenv import load_dotenv
+import os
 
-genai.configure(api_key="GEMINI_API_KEY")
+load_dotenv()
+
+GEMINI_API_KEY = os.environ.get('GEMINI_API_KEY')
+genai.configure(api_key=GEMINI_API_KEY)
 model = genai.GenerativeModel("gemini-1.5-flash")
 
 # Dependency for session management
