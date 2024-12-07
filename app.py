d286a7c5e55a01bd37c13b6a3598ddafde5d0917
jhernandez4 <jokhernandez120744@gmail.com>
2024-12-06 20:04:29 -0800

    Create login endpoint

--- app.py
+++ app.py
@@ -102,3 +102,20 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)], sessio
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.post("/login")
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    session: SessionDep) -> Token:
+    user = authenticate_user(form_data.username, form_data.password, session)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return Token(access_token=access_token, token_type="bearer")
